4e4556745b1b2838d83b4f1cc31c46cbe2c7a825
Hae-jin-park <74353756+Hae-jin-park@users.noreply.github.com>
2022-10-22 14:33:18 +0900

    Create ChildView.vue

---
+++ src/views/ChildView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>Child Component</h2>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: {
+      type: Number,
+      default: 0,
+    },
+  },
+};
+</script>
+
+<style></style>

e7acec3b57039391470187728983a0e0151ba528
Hae-jin-park <74353756+Hae-jin-park@users.noreply.github.com>
2022-10-22 14:18:11 +0900

    update

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
 import "./output.css";
+
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
   render: (h) => h(App),
 }).$mount("#app");
